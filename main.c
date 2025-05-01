e2c628cfc10703ae61778ae3bbc867dde86d55b2
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-05-01 13:14:51 -0300

    mudança no display

--- main.c
+++ main.c
@@ -147,24 +147,13 @@ void vDisplayTask() {
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
-    char str_y[5]; // Buffer para armazenar a string
-    char str_n[5];
-    int contador = 0;
     bool cor = true;
     while (true) {
-        sprintf(str_y, "%d", contador); // Converte em string
-        sprintf(str_n, "%d", night_mode);
-        contador++;                     // Incrementa o contador
         ssd1306_fill(&ssd, !cor);                          // Limpa o display
         ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
-        ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
-        ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
-        //ssd1306_draw_string(&ssd, "  FreeRTOS", 10, 28); // Desenha uma string
-        ssd1306_draw_string(&ssd, str_n, 10, 28); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Contador  LEDs", 10, 41);    // Desenha uma string
-        ssd1306_draw_string(&ssd, str_y, 40, 52);          // Desenha uma string
+        ssd1306_line(&ssd, 3, 20, 123, 20, cor);           // Desenha uma linha
+        ssd1306_draw_string(&ssd, "   SEMAFORO", 8, 6);
+        ssd1306_rect(&ssd, 95, 15, 100, 20, cor, !cor);    // Desenha um retângulo
         ssd1306_send_data(&ssd);                           // Atualiza o display
         sleep_ms(735);
     }
